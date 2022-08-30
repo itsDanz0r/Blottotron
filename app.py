41785358ca3ab1c5b682b80aeb2b59b975756726
Danny Kerr <danny.w.kerr@gmail.com>
2022-08-30 13:39:09 +0930

    Add ingredients menu, scrolling WIP

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from hardware_classes import *
 import kivy
 from kivy.app import App
 from kivy.uix.button import Button
@@ -6,32 +7,57 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import SlideTransition
 from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.image import Image
 
 Window.fullscreen = False
 Window.size = (800, 480)
 
-kivy.require('2.1.0')
+blottotron = Blottotron()
+ingredients_list = [Ingredient(name='Vodka'), Ingredient(name='Orange Juice')]
 
 
 class MasterLayout(BoxLayout):
-    def __init__(self, parent, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.label = Label(
+        self.heading = Label(
             text='BLOTTOTRON',
             font_size='40',
             font_name='ETHNOCENTRIC (TURBO COVERS)',
-            size_hint_y=0.3,
-            pos_hint={'y': 1}
+            size_hint_y=0.2
         )
-        self.add_widget(self.label)
+        self.bar_label = Label(
+            text="Your Bar:\n\nNONE",
+            font_size="20",
+            halign='center',
+            size_hint_y=0.3
+        )
+        self.add_widget(self.heading)
+        self.add_widget(self.bar_label)
         self.orientation = 'vertical'
+        self.screen_manager = ScreenManager()
+        self.add_screens()
+        self.add_widget(self.screen_manager)
+
+    def add_screens(self):
+        self.screen_manager.add_widget(MainMenu(parent=self.screen_manager))
+        self.screen_manager.add_widget(SettingsMenu(parent=self.screen_manager))
+        self.screen_manager.add_widget(IngredientsMenu(parent=self.screen_manager))
+        self.screen_manager.current = 'main'
+
+    def update_bar_label(self):
+        bar_string = 'Your Bar:\n\n'
+        if not blottotron.bar:
+            bar_string += 'Empty'
+        for ingredient in blottotron.bar:
+            bar_string += ingredient.name + ' '
+        bar_string.strip()
+        self.bar_label.text = bar_string
 
 
 class BlottotronScreen(Screen):
-    def __init__(self, parent, **kwargs):
-        super().__init__(**kwargs)
-        self.parent = parent
+    def __init__(self, **kwargs):
+        super().__init__()
         self.layout = BoxLayout()
         self.layout.orientation = 'horizontal'
         self.layout.padding = '50dp'
@@ -40,10 +66,10 @@ class BlottotronScreen(Screen):
         self.widgets = []
         self.add_widget(self.layout)
 
-
     def add_widgets_to_layout(self):
         for widget in self.widgets:
             self.layout.add_widget(widget)
+
 
 class MenuButton(Button):
     def __init__(self, **kwargs):
@@ -56,9 +82,9 @@ class MenuButton(Button):
 
 class MainMenu(BlottotronScreen):
     def __init__(self, **kwargs):
-        super().__init__(parent=None)
+        super().__init__()
         self.name = 'main'
-        self.size_hint_y = 1
+        self.size_hint_y = 0.5
         self.pour_button = MenuButton(
             text='\n\nDRINK',
         )
@@ -86,10 +112,11 @@ class MainMenu(BlottotronScreen):
 class SettingsMenu(BlottotronScreen):
 
     def __init__(self, **kwargs):
-        super().__init__(parent=None)
+        super().__init__()
         self.name = 'settings'
         self.ingredients_button = MenuButton(
             text='SELECT\nINGREDIENTS',
+            on_release=self.ingredients_menu_clicked
         )
         self.back_button = MenuButton(
             text='RETURN TO\nMENU',
@@ -105,18 +132,55 @@ class SettingsMenu(BlottotronScreen):
         self.parent.current = 'main'
         self.parent.transition = SlideTransition(direction='left')
 
+    def ingredients_menu_clicked(self, _=None):
+        self.parent.current = 'ingredients'
+
+
+class IngredientsMenu(BlottotronScreen):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.name = 'ingredients'
+        self.ingredient_buttons = []
+        self.layout = BoxLayout()
+        self.layout.orientation = 'vertical'
+        self.layout.padding = '30dp'
+        self.layout.spacing = '20dp'
+        self.layout.size_hint_y = None
+        self.layout.height = self.layout.minimum_height
+        for i in range(0, 10):
+            try:
+                self.ingredient_buttons.append(Button(
+                    text='Ingredient ' + str(i + 1) + ': ' + blottotron.bar[i].name,
+                    size_hint=(None, None),
+                    size=(400, 50),
+                    font_size=20
+                )
+                )
+            except IndexError:
+                self.ingredient_buttons.append(Button(
+                    text='Ingredient ' + str(i) + ': None',
+                    font_size=20,
+                    size_hint=(None, None),
+                    size=(400, 50),
+                )
+                )
+        self.scroll_view = ScrollView(size_hint=(1, None), size=self.layout.size)
+        self.scroll_view.do_scroll_x = False
+        self.scroll_view.do_scroll_y = True
+        self.add_widget(self.scroll_view)
+        self.scroll_view.add_widget(self.layout)
+        for button in self.ingredient_buttons:
+            self.layout.add_widget(button)
+
 
 class BlottotronApp(App):
     def build(self):
-        master_layout = MasterLayout(None)
-        screen = ScreenManager()
-        screen.add_widget(MainMenu(parent=screen))
-        screen.add_widget(SettingsMenu(parent=screen))
+        return MasterLayout()
 
-        screen.current = 'main'
-        master_layout.add_widget(screen)
-        return master_layout
+
+def main():
+    BlottotronApp().run()
 
 
 if __name__ == '__main__':
-    BlottotronApp().run()
+    main()
--- hardware_classes.py
+++ hardware_classes.py
@@ -1,11 +1,11 @@
 import sqlite3
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 
 
 class Blottotron:
     def __init__(self):
-        GPIO.setmode(GPIO.BOARD)
+        #GPIO.setmode(GPIO.BOARD)
         self.bar = []
 
     def setup_bar(self, ingredients_list):
@@ -29,20 +29,37 @@ class Ingredient:
         self.name = name
         self.type = ''
         self.pump = None
+        self.bottle_volume = 0
         self.volume_remaining = 0
 
     def pour(self, volume):
+
         run_time = volume * 150
         # Pour volume is ~150ml/s @ 490hz 25% DC
         self.pump.run_pump(run_time)
+
+    def percent_remaining(self):
+        return str(round(self.volume_remaining / self.bottle_volume, 3)) + '%'
+
+    def check_low_volume(self):
+        if self.percent_remaining() < 10:
+            return True
+        else:
+            return False
+
+    def check_sufficient_volume(self, volume):
+        if self.volume_remaining < volume:
+            return True
+        else:
+            return False
 
 
 class Pump:
     def __init__(self, pwm_pin):
         self.speed = 255
-        GPIO.setup(pwm_pin, GPIO.OUT)
+        #GPIO.setup(pwm_pin, GPIO.OUT)
         self.pwm_pin = pwm_pin
-        self.pwm = GPIO.PWM(self.pwm_pin, 490)
+        #self.pwm = GPIO.PWM(self.pwm_pin, 490)
 
     def run_pump(self, run_time):
         start_time = time.time()
